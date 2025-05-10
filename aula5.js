6dc78d24ba57fa0fe7d347387b367b8d873ffb64
gabrielp71 <gaabrielpereira2003@gmail.com>
2025-05-09 21:35:24 -0300

    JavaScript
    Salvamento

---
+++ aula5.js
@@ -0,0 +1,37 @@
+//BLOCO01
+let primeiroNome = 'John';
+let ultimoNome = 'Doe';
+let nomeCompleto = primeiroNome + ' ' + ultimoNome; // usando o +
+let nomeCompletoTemplate = `${primeiroNome} ${ultimoNome}`;
+console.log(nomeCompleto);
+
+
+//FIM DO BLOCO 01
+
+//crie, 3 blocos de variaveis que contenha 1-(Paises e Capitais), 2(tipos de dados bancarios), 3(renda familiar)
+
+//BLOCO02
+
+let primeiroPais = 'Brasil';
+let primeiroCapital = 'Brasilia';
+let completoBr = primeiroPais + ' ' + primeiroCapital;
+let completoBrTemplate = `Meu nome é Isabel nasci em ${primeiroPais} onde a capital é ${primeiroCapital}`;
+console.log(completoBr);
+
+let segundoPais = 'Israel';
+let segundoCapital = 'Jerusalém';
+let completoIs = segundoPais + ' ' + segundoCapital;
+let fraseCompleta = `Meu nome é Gabriel nasci em ${segundoPais} onde a capital é ${segundoCapital}`;
+console.log(fraseCompleta);
+
+//FIM DO BLOCO03
+
+//BLOCO03
+
+let nomeInteiro = 'Gabriel Pereira'
+let banco = 'Caixa'
+
+
+
+
+
