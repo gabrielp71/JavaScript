a152a550c9ce3be7be41b4a97351ef283c9e4aa8
gabrielp71 <gaabrielpereira2003@gmail.com>
2025-05-13 21:44:31 -0300

    JavaScript
    Salvamento

---
+++ aula08.js
@@ -0,0 +1,21 @@
+let x = 10;
+let y = '10';
+
+console.log(x === y);
+//ESTAMOS TESTANDO SE O VALOR DE "X" É IGUAL O VALOR DE "Y"
+// AS RESPOSTAS SERÃO TRUE (para verdadeiro/afirmação) FALSE (para falso/ negação)
+
+let frutas = "Banana";
+let frutas2 = "maça";
+//aqui estamos perguntando se o valor da variável "frutas"  é DIFERENTE da variável "frutas2"
+console.log(frutas !== frutas2);
+
+let numero = 20;
+let segundoNumero = 258;
+//Estamos utilizando (maior que > ) e (menor que < ) para comparar determinados informações
+console.log( numero > 30); // ( > ) MAIOR QUE, ( < ) MENOR QUE.
+
+
+
+
+
