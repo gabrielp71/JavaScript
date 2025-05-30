f8cf9dee072407df80db1d71e9f21c009b75b00c
gabrielp71 <gaabrielpereira2003@gmail.com>
2025-05-29 21:19:49 -0300

    JavaScript
    Salvamento

--- javascript aula13/script.js
+++ javascript aula13/script.js
@@ -5,3 +5,15 @@ function tornarCirculo() {
     meuElemento.classList.add("circle"); //adiciono uma class para arredondar o quadrado
     meuElemento.classList.remove("square"); // removo uma class para arredondar a caixa
 }
+
+function tornarQuadrado() {
+    meuElemento.classList.add("square");
+    meuElemento.classList.remove("circle");
+}
+
+function alternarFormato() {
+    meuElemento.classList.toggle("circle");
+    meuElemento.classList.toggle("square");
+}
+
+//toggle é responsavel por verificar o elemento atual (funciona como se fosse um vigia )
